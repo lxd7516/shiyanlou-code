03ffec9ff6919955b97bfb4d5edc9b176c1ef31a
lxd7516 <lxd18811508707@163.com>
2020-02-11 17:22:07 +0800

    two demo

---
+++ jump7.py
@@ -0,0 +1,11 @@
+for a in range(0,100):
+        if a % 7 == 0:
+                    pass
+                    elif a % 10 == 7:
+                                pass
+                                elif a // 10 == 7:
+                                            pass
+                                            else:
+                                                        print(a)
+
+
